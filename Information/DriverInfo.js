9ddf4dd08da9bfd0ae35b267b6d339bdb109c71c
mariellagh14 <galvezmariella@gmail.com>
2023-10-12 15:23:58 -0700

    Ella's branch

--- Information/DriverInfo.js
+++ Information/DriverInfo.js
@@ -1,6 +1,54 @@
 function DriverInfo(){
-    return
-    //licencse plate
-    //whole driver info
+ 
+/*
+    Profile Information:
+        Full Name
+        Profile Picture
+        Contact Information - Phone number
+    Driver's Licence Details:
+        Driver's License Number
+        License Expiration Date
+    Vehicle Information:
+        Vehicle Make and Model
+        Vehicle Year
+        License Plate Number
+        Vehicle Color
+    Insurance Information:
+        Proof of Vehicle Insurance
+    Ride Preference:
+        Availability days and times
+    Emergency Contact:
+        Name and Phone Number of Emergency Contact
+     */
+
+return {
+    /* Profile Information */
+    fullName: "",
+    profilePicture: "",
+    contactInformation: {
+        phoneNumber: ""
+    },
+    /* Driver's License Details */
+    driversLicenseDetails: {
+        licenseNumber: "",
+        expirationDate: ""
+    },
+    /* Vehicle Information */
+    vehicleInformation: {
+        makeAndModel: "",
+        year: "",
+        licensePlateNumber: "",
+        color: ""
+    },
+    /* Insurance Information */
+    proofOfInsurance: "",
+    /* Ride Preference */
+    availability: "",
+    /* Emergency Contact */
+    emergencyContact: {
+        name: "",
+        phoneNumber: ""
+    }
+    }; 
 }
 export default DriverInfo;
--- Screens /Map.js
+++ Screens /Map.js
@@ -1,5 +1,41 @@
-function Maps(){
-    return 
+import React from 'react';
+import { Text, View, TouchableOpacity, Linking } from 'react-native';
+
+function Maps() {
+  const handleButtonPress = () => {
     
+    console.log('Button Pressed');
+    
+    // Open Google Maps with a specific location (you can replace the coordinates)
+    const latitude = 37.7749; // Replace with the desired latitude
+    const longitude = -122.4194; // Replace with the desired longitude
+    const label = 'Google Maps Label'; // Replace with a label for the location
+
+    // Construct the Google Maps URL
+    const googleMapsUrl = `https://www.google.com/maps?q=${latitude},${longitude}&label=${label}`;
+
+    // Open the Google Maps app or a web browser with the URL
+    Linking.openURL(googleMapsUrl).catch((err) => console.error('An error occurred: ', err));
+  };
+
+  return (
+    <View>
+      <Text>
+        This is where the map showing where drivers are located
+      </Text>
+      <TouchableOpacity onPress={handleButtonPress}>
+        <View
+          style={{
+            backgroundColor: 'yellow', // Button background color
+            padding: 10, // Adjust the padding as needed
+            borderRadius: 5, // Adjust the border radius as needed
+          }}
+        >
+          <Text style={{ color: 'white' }}>Open Google Maps</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
 export default Maps;
