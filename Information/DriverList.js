2951b78452c0f0afd46d41ea6fb400c15a0f8286
Ali Eslami <alinox360@gmail.com>
2023-10-17 14:09:55 -0700

    FIX

---
+++ Information/DriverList.js
@@ -0,0 +1,7 @@
+import {FlatList} from'react-native';
+
+
+function Driverlist({driver}){
+    return <FlatList />;
+}
+export default Driverlist;
---
+++ Information/DriverSummary.js
@@ -0,0 +1,16 @@
+
+import {View,Text}from 'react-native';
+
+function DriverSummary({driver}){
+    return(
+        <View>
+            <Text>
+                {driver}
+
+            </Text>
+        </View>
+        
+
+    );
+}
+export default DriverSummary;
