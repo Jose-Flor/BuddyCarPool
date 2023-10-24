187c6ed8905bd2f47767b2ee942632085df00a65
mariellagh14 <galvezmariella@gmail.com>
2023-10-24 14:35:28 -0700

    bella

--- Screens /Map.js
+++ Screens /Map.js
@@ -1,19 +1,5 @@
-import MapScreen from './MapScreen';
-
 function Maps(){
-    return (
-        <View style={{ flex: 1 }}>
-          <MapView
-            style={{ flex: 1 }}
-            initialRegion={{
-              latitude:  34.2397,
-              longitude: -118.5291,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          />
-        </View>
-      );
-    }
+    return 
     
-    export default MapScreen;
+}
+export default Maps;
