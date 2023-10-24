7c057e7eabde71fd7a1fdfd2aec208ec60931388
Ali Eslami <alinox360@gmail.com>
2023-10-24 14:29:11 -0700

    my changes

--- Screens /RegisterForm.js
+++ Screens /RegisterForm.js
@@ -4,42 +4,152 @@ import {StyleSheet, Text, TextInput, View}from'react-native';
 
 
 
-function RegisterForm({isLogin,onSubmit,credentialsInvalid}){
-    const [firstName,setFistName]=useState('');
-    const [lastName,setLastName]=useState('');
-    const[enteredEmil,setEnteredEmil]=useState('');
-    const[enteredConfirmEmail,setEnteredConfirmEmail]=useState('');
-    const[enterPassword,setEnterPassword]=useState('');
-    const[enteredConfirmPassword,setEntereConfirmPassword]=useState('');
-
+function RegisterForm({ isLogin, onSubmit, credentialsInvalid={} }) {
+    const [firstName, setFirstName] = useState(''); 
+    const [lastName, setLastName] = useState(''); 
+    const [enteredEmail, setEnteredEmail] = useState(''); 
+    const [enteredConfirmEmail, setEnteredConfirmEmail] = useState(''); 
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [enteredConfirmPassword, setEnteredConfirmPassword] = useState(''); 
+  
     const {
-        email:emailIsInavlid,
-        confirmEmail:emailsDontMatch,
-        password:passwordIsInavlid,
-        confirmPassword:passwordDontMatch,
-    }=credentialsInvalid;
-    
-   
+      email: emailIsInvalid, 
+      confirmEmail: emailsDontMatch,
+      password: passwordIsInvalid, 
+      confirmPassword: passwordsDontMatch, 
+    } = credentialsInvalid
+    function updateInputValueHnadler(inputType,enteredValue){
+        switch(inputType){
+            case'firstName':
+            setFirstName(enteredValue);
+            break;
+            case'lastName':
+            setLastName(enteredValue);
+            break;
+            case'email':
+            setEnteredEmail(enteredValue);
+            break;
+            case'confirmEmail':
+            setEnteredConfirmEmail(enteredValue);
+            break;
+            case'password':
+            setEnteredPassword(enteredValue);
+            break;
+            case'passwordconfirmation':
+            setEnteredConfirmPassword(enteredValue);
+            break;
+
+    }
+
+}
+function submitHnadler(){
+    onSubmit({
+        firstName,
+        lastName,
+        email:enteredEmail,
+        confirmEmail:enteredConfirmEmail,
+        password:enteredPassword,
+        confirmPassword:enteredConfirmPassword,
+    })
+}
+
+   //
   
     return (
     <View style={style.container}>
        <Text style={style.Header}>What is you name </Text>
-       <View style={style.SubHeader}>
+       <View style={style.SubHeader}> 
+       </View>
         <Text>Name</Text>
         <TextInput 
-        style={[]}
+        
         autoCapitalize='none'
         placeholder="Enter your name"
+        onChangeText={(text) => updateInputValueHnadler('firstName', text)}
+        value={firstName}
+        style={style.inputContainer}
         
         
         />
-        <View >
+        <Text>LastName</Text>
+        <TextInput
+        autoCapitalize='none'
+        placeholder="Enter your Last name"
+        onChangeText={(text) => updateInputValueHnadler('LastName',text)}
+        value={lastName}
+        style={style.inputContainer}
 
-        </View>
+        />
+        <View>
+
+       <Text style={[style.label,emailIsInvalid && style.labelIsInvalid]}>Email Address</Text>
+       <TextInput 
+       autoCapitalize='none'
+       keyboardType='email-address'
+       onChangeText={(text) => updateInputValueHnadler('Email',text)}
+       value={enteredEmail}
+       style={[style.inputContainer, emailIsInvalid&&style.inputIsInvalid]}
+       />
+       </View>
+       {!isLogin && (
+        <View>
+            <Text style={[style.label,emailsDontMatch && style.labelIsInvalid]}>
+                confirm email address
+            </Text>
+            <TextInput
+            autoCapitalize='none'
+            keyboardType='email-address'
+            onChangeText={(text) => updateInputValueHnadler('confirmEmail',text)}
+            value={enteredConfirmEmail}
+            style={[style.inputContainer,emailsDontMatch && style.inputIsInvalid]}
+            />
+            </View>
+            )}
+            <View>
+                <Text style={[style.label , passwordIsInvalid && style.labelIsInvalid]}>
+                    Password
+                </Text>
+                <TextInput
+                autoCapitalize='none'
+                onChangeText={(text) => updateInputValueHnadler('password',text)}
+                value={enteredPassword}
+                style={[style.inputContainer , passwordIsInvalid && style.labelIsInvalid]}
+                />
+             </View>
+             {!isLogin &&(
+                <View> 
+                    <Text style={[style.label ,passwordsDontMatch && style.labelIsInvalid]}>
+                        confirm Password
+
+                   </Text>
+                   <TextInput 
+                   autoCapitalize='none'
+                   onChangeText={(text) => updateInputValueHnadler ('confirmPasswords', text)}
+                   value={enteredConfirmPassword}
+                   style={[style.inputContainer, passwordsDontMatch,style.inputIsInvalid]}
+                   
+                   />
+                </View>
+             )}
+             <View>
+            
+             </View>
+             
+
+
+
+
+        
+    
+
+
+
+       
+       <TextInput/>
 
        </View>
 
-    </View>
+    
 
     );
 
@@ -64,12 +174,33 @@ const style=StyleSheet.create({
     },
     inputContainer:{
         borderWidth:1,
-        borderColor:'#FFF'
+        borderColor:'#FFF',
+        borderRadius:5,
+        padding:10,
+        marginBottom:15,
+        color:'#FFF',
+        
     },
     SubHeader:{
         fontSize:16,
         marginBottom:20,
-        color:'#FFF',
+        color:'black',
+    },
+    inputIsInvalid:{
+
+    },
+    label:{
+
+    },
+    labelIsInvalid:{
+
+    },
+    submitHandle:{
+
+    },
+    button:{
+
     }
+    
         
     })
