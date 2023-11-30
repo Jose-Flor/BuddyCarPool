0e24bf4120b59141c596dd6d1142330eba72bbc5
Ali Eslami <alinox360@gmail.com>
2023-11-30 14:11:31 -0800

    fix merge

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import RegisterForm from './Screens /RegisterForm';
 import Driverlist from './Information/DriverList';
 import DriverOverView from './Information/DriverOverView';
+import ScheduleLocation from './Screens /ScheduleLocation';
 
 const Stack=createNativeStackNavigator();
 
@@ -16,15 +17,23 @@ export default function App() {
 
       <Stack.Navigator initialRouteName='Register'>
         <Stack.Screen name='Register' component={RegisterScreen} options={{headerShown:false}}/>
+        
         <Stack.Screen  name='main' component={MainScreen} />
-        <Stack.Screen name='DriverOverView' component={DriverOverView}/>
+        <Stack.Screen name='DriverOverView' component={DriverOverView}
+        options={{
+          presentation:'modal'
+        }}
+        />
         <Stack.Screen name='RegisterForm' component={RegisterForm}/>
+        <Stack.Screen  name='ScheduleLocation' component={ScheduleLocation}/>
+        <Stack.Screen name='DriverList' component={Driverlist} />
+        
 
       </Stack.Navigator>
     </NavigationContainer> 
   );
 }
-
+//later add option={{headerhown:false}} to remove the header
 const styles = StyleSheet.create({
   container: {
    flex: 1,
