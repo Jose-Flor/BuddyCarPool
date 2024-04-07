d960ca454e02624f77161fd756d0c65b07a7880b
Jose Flores <floresjose6829@gmail.com>
2024-04-07 10:51:58 -0700

    adding feature of allowing sharing location on the maps page

--- Screens /Map.js
+++ Screens /Map.js
@@ -1,5 +1,28 @@
-function Maps(){
-    return 
-    
+import React, { useEffect } from 'react';
+import { View, Button, Alert } from 'react-native';
+import * as Location from 'expo-location';
+
+function Maps() {
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  const getLocationPermission = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission Denied', 'You need to enable location permissions to use this feature.');
+      }
+    } catch (error) {
+      console.error('Error requesting location permission:', error);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Allow Location Access" onPress={getLocationPermission} />
+    </View>
+  );
 }
+
 export default Maps;
